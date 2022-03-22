73c54bb2965eda920c6dc671612b1677877f1cd8
forfeen <f.fin07@gmail.com>
2022-03-22 19:16:11 +0700

    💄 add charts

--- src/screens/services/vaccine.service.ts
+++ src/screens/services/vaccine.service.ts
@@ -1,5 +1,9 @@
 import baseURL from '../../api/http-common';
+import covidURl from '../../api/covid-http';
+import vaccinationUrl from '../../api/vaccination-http';
 import Vaccine from '../../types/vaccine.type';
+import VaccinationData from '../../types/vaccination.type';
+import Covid from '../../types/covid.type';
 import axios, {AxiosResponse} from 'axios';
 
 class VaccineDataService {
@@ -22,6 +26,18 @@ class VaccineDataService {
 
     getPost(id) {
         return axios.get<Vaccine>(baseURL + '/timelines/'+id);
+    }
+
+    getCovidCase() {
+        return axios.get<Covid>(covidURl);
+    }
+
+    // getVaccinationData() {
+    //     return axios.get<VaccinationData>(vaccinationUrl + 'fullData=false');
+    // }
+
+    getVaccinationFullData() {
+        return axios.get<VaccinationData>(vaccinationUrl);
     }
 
 }
