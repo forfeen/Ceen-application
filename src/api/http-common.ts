a2664876149d8308942219daf4a6d025fddc7307
forfeen <f.fin07@gmail.com>
2022-04-19 21:34:36 +0700

    change baseURL

--- src/api/http-common.ts
+++ src/api/http-common.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const baseURL = axios.create({
-	url: 'https://script.google.com/macros/s/AKfycbxbeeK5D_Iwq0SzbN49v0nY9hDnVj7ekBk5BJHRgEjugOiNLTcRLF6YfuKNC7BjXhNI/exec?path='
+	url: 'https://script.google.com/macros/s/AKfycbwMTbzXFTgwl1AqS4PT-ndqp4rH_wpPoCXk3KgfYKuYUmaOUEh44pclRNdYMfWj24ns/exec?path='
 });
 
 export default baseURL.defaults.url;
