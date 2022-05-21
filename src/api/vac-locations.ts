1e83ca1005305f52e5eede86c01256031400073b
forfeen <f.fin07@gmail.com>
2022-05-21 19:40:12 +0700

    add limit on map api

--- src/api/vac-locations.ts
+++ src/api/vac-locations.ts
@@ -4,7 +4,7 @@ import baseURL from '../api/http-common';
 const vacLocationsURL = axios.create({
 	// status: available = 0
 	// url: 'https://jitasa.care/api/v2/places?type=6&status=0'
-	url: baseURL+'/medical-centers'
+	url: baseURL+'/medical-centers&limit=3896&offset=0'
 });
 
 export default vacLocationsURL.defaults.url;
